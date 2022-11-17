c7d6c796beca6349dc97d6923347ef0380e8020b
ducloc0009 <ducloc0009@gmail.com>
2022-11-17 16:32:36 +0700

    test: grouping test cases, add call api test with non-existed email and wrong password, add more assertions
    Assertions: check api call, arguments, call times, check loading

--- src/pages/Login/__tests__/Login.test.tsx
+++ src/pages/Login/__tests__/Login.test.tsx
@@ -1,4 +1,4 @@
-import { screen } from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { setupServer } from "msw/node";
 import { rest } from "msw";
@@ -7,128 +7,246 @@ import { renderWithProviders } from "utils/test.utils";
 import Login from "../Login";
 import Message from "components/Common/Message";
 import { store } from "redux/store";
+import * as action from "redux/actions/user.action";
 
 const buildLoginData = () => ({
   email: faker.internet.email(),
   password: faker.internet.password(),
 });
 
-const localStorageMock = (function () {
-  let store: any = {};
-  return {
-    getItem: function (key: string) {
-      return store[key] || null;
-    },
-    setItem: function (key: string, value: any) {
-      store[key] = value.toString();
-    },
-    removeItem: function (key: string) {
-      delete store[key];
-    },
-    clear: function () {
-      store = {};
-    },
-  };
-})();
+describe("login validate", () => {
+  test("login without input value", async () => {
+    renderWithProviders(<Login />, { user: { isLoggedIn: false } });
 
-Object.defineProperty(window, "localStorage", {
-  value: localStorageMock,
-});
+    await userEvent.click(screen.getByRole("button", { name: /login/i }));
 
-const server = setupServer(
-  rest.post(`http://localhost:5000/auth`, (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json({
-        access_token: "123456",
-      })
+    expect(screen.getByText(/please enter your email/i)).toBeInTheDocument();
+    expect(screen.getByText(/please enter your password/i)).toBeInTheDocument();
+  });
+
+  test("login with wrong format email and password", async () => {
+    renderWithProviders(<Login />, { user: { isLoggedIn: false } });
+
+    const data = {
+      email: "wrongEmailFormat",
+      password: "123",
+    };
+
+    await userEvent.type(screen.getByPlaceholderText(/email/i), data.email);
+    await userEvent.type(
+      screen.getByPlaceholderText(/password/i),
+      data.password
     );
-  }),
 
-  rest.get(
-    `${process.env.REACT_APP_BACK_END_URL}/users/me`,
-    (req, res, ctx) => {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          id: "1",
-          name: "Loc",
-        })
-      );
-    }
-  )
-);
+    await userEvent.click(screen.getByRole("button", { name: /login/i }));
 
-beforeAll(() => server.listen());
-beforeEach(() => server.resetHandlers());
-afterAll(() => server.close());
-
-test("login successfully", async () => {
-  const spyLocalStorage = jest.spyOn(localStorage, "removeItem");
-  renderWithProviders(<Login />, { user: { isLoggedIn: false } });
-
-  const { email, password } = buildLoginData();
-  await userEvent.type(screen.getByPlaceholderText(/email/i), email);
-  await userEvent.type(screen.getByPlaceholderText(/password/i), password);
-
-  await userEvent.click(screen.getByRole("button", { name: /login/i }));
-
-  expect(spyLocalStorage).toHaveBeenCalled();
-  const pathName = window.location.pathname;
-  expect(pathName).toBe("/");
+    expect(screen.getByText(/email is invalid/i)).toBeInTheDocument();
+    expect(
+      screen.getByText(/password should be at least 6 characters/i)
+    ).toBeInTheDocument();
+  });
 });
 
-test("login without input value", async () => {
-  renderWithProviders(<Login />, { user: { isLoggedIn: false } });
+describe("login with API call", () => {
+  const server = setupServer(
+    rest.post<{ email: string; password: string }>(
+      `${process.env.REACT_APP_BACK_END_URL}/auth`,
+      (req, res, ctx) => {
+        const { password, email } = req.body;
 
-  await userEvent.click(screen.getByRole("button", { name: /login/i }));
+        if (!email)
+          return res(
+            ctx.status(401),
+            ctx.json({ message: "Email is missing" })
+          );
 
-  expect(screen.getByText(/please enter your email/i)).toBeInTheDocument();
-  expect(screen.getByText(/please enter your password/i)).toBeInTheDocument();
-});
+        if (!password)
+          return res(
+            ctx.status(401),
+            ctx.json({ message: "Email is missing" })
+          );
+        return res(
+          ctx.status(200),
+          ctx.json({
+            access_token: "123456",
+          })
+        );
+      }
+    ),
 
-test("login with wrong format email and password", async () => {
-  renderWithProviders(<Login />, { user: { isLoggedIn: false } });
-
-  const data = {
-    email: "wrongEmailFormat",
-    password: "123",
-  };
-
-  await userEvent.type(screen.getByPlaceholderText(/email/i), data.email);
-  await userEvent.type(screen.getByPlaceholderText(/password/i), data.password);
-
-  await userEvent.click(screen.getByRole("button", { name: /login/i }));
-
-  expect(screen.getByText(/email is invalid/i)).toBeInTheDocument();
-  expect(
-    screen.getByText(/password should be at least 6 characters/i)
-  ).toBeInTheDocument();
-});
-
-test("login with wrong email and password", async () => {});
-
-test("unexpected error", async () => {
-  const errorMessage = "Oh no server error";
-  server.use(
-    rest.post("http://localhost:5000/auth", async (req, res, ctx) => {
-      return res(ctx.status(500), ctx.json({ message: errorMessage }));
-    })
+    rest.get(
+      `${process.env.REACT_APP_BACK_END_URL}/users/me`,
+      (req, res, ctx) => {
+        return res(
+          ctx.status(200),
+          ctx.json({
+            id: "1",
+            name: "Loc",
+          })
+        );
+      }
+    )
   );
-  renderWithProviders(
-    <>
-      <Message />
-      <Login />
-    </>,
-    null,
-    store
-  );
-  const { email, password } = buildLoginData();
-  await userEvent.type(screen.getByPlaceholderText(/email/i), email);
-  await userEvent.type(screen.getByPlaceholderText(/password/i), password);
 
-  await userEvent.click(screen.getByRole("button", { name: /login/i }));
+  beforeAll(() => {
+    server.listen();
+  });
+  afterEach(() => {
+    server.resetHandlers();
+  });
+  afterAll(() => {
+    server.close();
+  });
 
-  // eslint-disable-next-line testing-library/no-debugging-utils
-  screen.debug();
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("login successfully", async () => {
+    renderWithProviders(
+      <>
+        <Message />
+        <Login />
+      </>,
+      undefined,
+      store
+    );
+    const loginSpy = jest.spyOn(action, "login");
+
+    const { email, password } = buildLoginData();
+    await userEvent.type(screen.getByPlaceholderText(/email/i), email);
+    await userEvent.type(screen.getByPlaceholderText(/password/i), password);
+
+    await userEvent.click(screen.getByRole("button", { name: /login/i }));
+
+    await waitFor(() => {
+      expect(
+        screen.getByRole("button", { name: /login/i })
+      ).toBeInTheDocument();
+    });
+
+    expect(loginSpy).toBeCalledTimes(1);
+    expect(loginSpy).toBeCalledWith(email, password);
+    const pathName = window.location.pathname;
+    loginSpy.mockRestore();
+
+    expect(pathName).toBe("/");
+  });
+
+  test("login with unregistered email", async () => {
+    server.use(
+      rest.post<{ email: string; password: string }>(
+        `${process.env.REACT_APP_BACK_END_URL}/auth`,
+        async (req, res, ctx) => {
+          const { email } = req.body;
+          if (email !== "email@example.com")
+            return res(
+              ctx.status(401),
+              ctx.json({ message: "Email is not existed" })
+            );
+        }
+      )
+    );
+
+    renderWithProviders(
+      <>
+        <Message />
+        <Login />
+      </>,
+      undefined,
+      store
+    );
+    const { email, password } = buildLoginData();
+    await userEvent.type(screen.getByPlaceholderText(/email/i), email);
+    await userEvent.type(screen.getByPlaceholderText(/password/i), password);
+
+    await userEvent.click(screen.getByRole("button", { name: /login/i }));
+
+    await waitFor(() => {
+      expect(
+        screen.getByRole("button", { name: /login/i })
+      ).toBeInTheDocument();
+    });
+
+    await waitFor(() => {
+      expect(screen.getByText("401: Email is not existed")).toBeInTheDocument();
+    });
+  });
+
+  test("login with wrong password", async () => {
+    server.use(
+      rest.post<{ email: string; password: string }>(
+        `${process.env.REACT_APP_BACK_END_URL}/auth`,
+        async (req, res, ctx) => {
+          const { password } = req.body;
+          if (password !== "123456")
+            return res(
+              ctx.status(401),
+              ctx.json({ message: "Password is incorrect" })
+            );
+        }
+      )
+    );
+
+    renderWithProviders(
+      <>
+        <Message />
+        <Login />
+      </>,
+      undefined,
+      store
+    );
+    const { email, password } = buildLoginData();
+    await userEvent.type(screen.getByPlaceholderText(/email/i), email);
+    await userEvent.type(screen.getByPlaceholderText(/password/i), password);
+
+    await userEvent.click(screen.getByRole("button", { name: /login/i }));
+
+    await waitFor(() => {
+      expect(
+        screen.getByRole("button", { name: /login/i })
+      ).toBeInTheDocument();
+    });
+
+    await waitFor(() => {
+      expect(
+        screen.getByText("401: Password is incorrect")
+      ).toBeInTheDocument();
+    });
+  });
+
+  test("unexpected error", async () => {
+    const errorMessage = "Oh no server error";
+    server.use(
+      rest.post<{ email: string; password: string }>(
+        `${process.env.REACT_APP_BACK_END_URL}/auth`,
+        async (req, res, ctx) => {
+          return res(ctx.status(500), ctx.json({ message: errorMessage }));
+        }
+      )
+    );
+    renderWithProviders(
+      <>
+        <Message />
+        <Login />
+      </>,
+      undefined,
+      store
+    );
+    const { email, password } = buildLoginData();
+    await userEvent.type(screen.getByPlaceholderText(/email/i), email);
+    await userEvent.type(screen.getByPlaceholderText(/password/i), password);
+
+    await userEvent.click(screen.getByRole("button", { name: /login/i }));
+
+    await waitFor(() => {
+      expect(
+        screen.getByRole("button", { name: /login/i })
+      ).toBeInTheDocument();
+    });
+
+    await waitFor(() => {
+      expect(screen.getByText("500: Oh no server error")).toBeInTheDocument();
+    });
+  });
 });
