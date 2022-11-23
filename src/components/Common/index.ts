4b77f74b18b39385c25f4eef2ecf2de3033cabea
khuctrang <khuctrang1812@gmail.com>
2022-11-23 17:46:28 +0700

    chore: config eslint and resolve some errors

--- src/components/Common/index.ts
+++ src/components/Common/index.ts
@@ -1,8 +1,11 @@
-import Header from "components/Common/Header/Header";
-import Layout from "components/Common/Layout/Layout";
-import Sidebar from "components/Common/Sidebar/Sidebar";
-import Message from "components/Common/Message";
-import InlineError from "components/Common/InlineError";
-import PageWithTable from "components/Common/PageWithTable/PageWithTable";
-import Modal from "components/Common/Modal/Modal";
-export { Header, Layout, Sidebar, Message, InlineError, PageWithTable, Modal };
+import Header from 'components/Common/Header/Header';
+import Layout from 'components/Common/Layout/Layout';
+import Sidebar from 'components/Common/Sidebar/Sidebar';
+import Message from 'components/Common/Message';
+import InlineError from 'components/Common/InlineError';
+import PageWithTable from 'components/Common/PageWithTable/PageWithTable';
+import Modal from 'components/Common/Modal/Modal';
+
+export {
+  Header, Layout, Sidebar, Message, InlineError, PageWithTable, Modal,
+};
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,5 @@
-import useAppDispatch from "hooks/useAppDispatch";
-import useAppSelector from "hooks/useAppSelector";
-import useTypedDispatch from "hooks/useTypedDispatch";
+import useAppDispatch from 'hooks/useAppDispatch';
+import useAppSelector from 'hooks/useAppSelector';
+import useTypedDispatch from 'hooks/useTypedDispatch';
+
 export { useAppDispatch, useAppSelector, useTypedDispatch };
