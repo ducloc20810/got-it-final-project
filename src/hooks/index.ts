014e1ae70927a26aa25eba345eb19793b2557608
ducloc0009 <ducloc0009@gmail.com>
2022-11-24 15:30:59 +0700

    refactor: delete unused file

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,4 @@
-import useAppDispatch from 'hooks/useAppDispatch';
 import useAppSelector from 'hooks/useAppSelector';
 import useTypedDispatch from 'hooks/useTypedDispatch';
 
-export { useAppDispatch, useAppSelector, useTypedDispatch };
+export { useAppSelector, useTypedDispatch };
--- src/hooks/useAppDispatch.ts
+++
@@ -1,6 +0,0 @@
-import { useDispatch } from 'react-redux';
-import type { AppDispatch } from 'redux/store';
-
-// Use throughout your app instead of plain `useDispatch` and `useSelector`
-const useAppDispatch: () => AppDispatch = useDispatch;
-export default useAppDispatch;
