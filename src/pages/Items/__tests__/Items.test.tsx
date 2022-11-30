30f844f760c71a2a8480bfdac9b2686802a0d32b
ducloc0009 <ducloc0009@gmail.com>
2022-11-30 16:56:55 +0700

    test: Add create item test cases

--- src/pages/Items/__tests__/Items.test.tsx
+++ src/pages/Items/__tests__/Items.test.tsx
@@ -1,27 +1,28 @@
+/* eslint-disable camelcase */
 import { waitFor, screen } from '@testing-library/react';
-import { rest } from 'msw';
-import { setupServer } from 'msw/lib/node';
 import { Route, Routes } from 'react-router-dom';
+import { setupServer } from 'msw/lib/node';
+import userEvent from '@testing-library/user-event';
+import { rest } from 'msw';
+import { Message, Modal } from 'components/Common';
 import * as itemAction from 'redux/actions/item.action';
 import { store } from 'redux/store';
 import { renderWithProviders } from 'utils/test.utils';
 import Items from '../Items';
+
+const categoryId = '1';
 
 const server = setupServer(
   rest.get(`${process.env.REACT_APP_BACK_END_URL}/categories/:categoryId`, (req, res, ctx) =>
     res(
       ctx.status(200),
       ctx.json({
-        total_items: 2,
-        items: [
-          {
-            id: 1,
-            name: 'Category 1',
-            description: 'Category 1 description',
-            img_url: 'url',
-          },
 
-        ],
+        id: 1,
+        name: 'Category 1',
+        description: 'Category 1 description',
+        img_url: 'url',
+
       }),
     )),
   rest.get(`${process.env.REACT_APP_BACK_END_URL}/categories/:categoryId/items`, (req, res, ctx) =>
@@ -52,6 +53,28 @@ const server = setupServer(
         ],
       }),
     )),
+
+  rest.post<{ name: string; image_url: string; description: string }>(
+    `${process.env.REACT_APP_BACK_END_URL}/categories`,
+    (req, res, ctx) => {
+      const { image_url, description } = req.body;
+
+      if (!image_url) return res(ctx.status(400), ctx.json({ message: 'Image url is missing' }));
+      if (!description) {
+        return res(ctx.status(400), ctx.json({ message: 'Description is missing' }));
+      }
+
+      return res(
+        ctx.status(201),
+        ctx.json({
+          id: 3,
+
+          image_url,
+          description,
+        }),
+      );
+    },
+  ),
 );
 
 window.scrollTo = jest.fn();
@@ -74,7 +97,6 @@ describe('fetch item list data', () => {
   test('fetch successfully with data', async () => {
     const fetchItemSpy = jest.spyOn(itemAction, 'fetchItemList');
     const pageNumber = 1;
-    const categoryId = '1';
 
     renderWithProviders(
       <Routes>
@@ -101,24 +123,25 @@ describe('fetch item list data', () => {
 
   test('fetch successfully with empty data', async () => {
     server.use(
-      rest.get(`${process.env.REACT_APP_BACK_END_URL}/categories/:categoryId/items`, (req, res, ctx) =>
-        res(
-          ctx.status(200),
-          ctx.json({
-            total_items: 0,
-            items: [],
-          }),
-        )),
+      rest.get(
+        `${process.env.REACT_APP_BACK_END_URL}/categories/:categoryId/items`,
+        (req, res, ctx) =>
+          res(
+            ctx.status(200),
+            ctx.json({
+              total_items: 0,
+              items: [],
+            }),
+          ),
+      ),
     );
 
     const fetchItemSpy = jest.spyOn(itemAction, 'fetchItemList');
     const pageNumber = 1;
-    const categoryId = '1';
 
     renderWithProviders(
       <Routes>
         <Route path="/categories/:categoryId/items" element={<Items />} />
-        ,
       </Routes>,
       {},
       store,
@@ -132,5 +155,106 @@ describe('fetch item list data', () => {
     });
 
     fetchItemSpy.mockRestore();
+  });
+});
+
+describe('create item', () => {
+  describe('open modal', () => {
+    test('user is logged in', async () => {
+      renderWithProviders(
+        <>
+          <Modal />
+          <Message />
+          <Routes>
+            <Route path="/categories/:categoryId/items" element={<Items />} />
+          </Routes>
+          ,
+        </>,
+        { user: { isLoggedIn: true } },
+        undefined,
+        `/categories/${categoryId}/items`,
+      );
+
+      await waitFor(() => {
+        expect(
+          screen.getByRole('columnheader', {
+            name: /id/i,
+          }),
+        ).toBeInTheDocument();
+      });
+
+      await userEvent.click(screen.getByRole('button', { name: /create item/i }));
+
+      await waitFor(() => {
+        expect(screen.getByText(/create item form/i)).toBeInTheDocument();
+      });
+    });
+
+    test('user is not logged in', async () => {
+      renderWithProviders(
+        <>
+          <Modal />
+          <Message />
+          <Routes>
+            <Route path="/categories/:categoryId/items" element={<Items />} />
+          </Routes>
+          ,
+        </>,
+        { user: { isLoggedIn: false } },
+        undefined,
+        `/categories/${categoryId}/items`,
+      );
+
+      await waitFor(() => {
+        expect(
+          screen.getByRole('columnheader', {
+            name: /id/i,
+          }),
+        ).toBeInTheDocument();
+      });
+
+      await userEvent.click(screen.getByRole('button', { name: /create item/i }));
+
+      await waitFor(() => {
+        expect(screen.getByText(/authentication warning/i)).toBeInTheDocument();
+      });
+    });
+  });
+
+  describe('form validation', () => {
+    test('submit with empty inputs', async () => {
+      renderWithProviders(
+        <>
+          <Modal />
+          <Message />
+          <Routes>
+            <Route path="/categories/:categoryId/items" element={<Items />} />
+          </Routes>
+          ,
+        </>,
+        { user: { isLoggedIn: true } },
+        undefined,
+        `/categories/${categoryId}/items`,
+      );
+
+      await waitFor(() => {
+        expect(
+          screen.getByRole('columnheader', {
+            name: /id/i,
+          }),
+        ).toBeInTheDocument();
+      });
+
+      await userEvent.click(screen.getByRole('button', { name: /create item/i }));
+
+      await waitFor(() => {
+        expect(screen.getByText(/create item form/i)).toBeInTheDocument();
+      });
+
+      await userEvent.click(screen.getByRole('button', { name: /submit/i }));
+
+      expect(screen.getByText(/please enter your item image url/i));
+      expect(screen.getByText(/please enter your item description/i));
+    });
   });
 });
