2d544e2c6486273516844a774defc51028faf41c
ducloc0009 <ducloc0009@gmail.com>
2022-12-01 12:47:22 +0700

    test: Add edit/remove item test cases

--- src/pages/Items/__tests__/Items.test.tsx
+++ src/pages/Items/__tests__/Items.test.tsx
@@ -8,6 +8,7 @@ import { Message, Modal } from 'components/Common';
 import * as itemAction from 'redux/actions/item.action';
 import { store } from 'redux/store';
 import { renderWithProviders } from 'utils/test.utils';
+import { TEST_IMAGE_URL } from 'constants/test';
 import Items from '../Items';
 
 const categoryId = '1';
@@ -54,7 +55,7 @@ const server = setupServer(
       }),
     )),
 
-  rest.post<{ name: string; image_url: string; description: string }>(
+  rest.post<{ image_url: string; description: string }>(
     `${process.env.REACT_APP_BACK_END_URL}/categories`,
     (req, res, ctx) => {
       const { image_url, description } = req.body;
@@ -75,6 +76,29 @@ const server = setupServer(
       );
     },
   ),
+
+  rest.put<{ image_url: string; description: string }>(
+    `${process.env.REACT_APP_BACK_END_URL}/categories/:categoryId/items/:itemId`,
+    (req, res, ctx) => {
+      const { image_url, description } = req.body;
+
+      if (!image_url) return res(ctx.status(400), ctx.json({ message: 'Image url is missing' }));
+      if (!description) {
+        return res(ctx.status(400), ctx.json({ message: 'Description is missing' }));
+      }
+
+      return res(
+        ctx.status(201),
+        ctx.json({
+          image_url,
+          description,
+        }),
+      );
+    },
+  ),
+
+  rest.delete(`${process.env.REACT_APP_BACK_END_URL}/categories/:categoryId/items/:itemId`, (req, res, ctx) =>
+    res(ctx.status(200), ctx.json({}))),
 );
 
 window.scrollTo = jest.fn();
@@ -255,6 +279,241 @@ describe('create item', () => {
 
       expect(screen.getByText(/please enter your item image url/i));
       expect(screen.getByText(/please enter your item description/i));
+    });
+  });
+});
+
+describe('edit item', () => {
+  describe('open modal', () => {
+    test('user is logged in', async () => {
+      renderWithProviders(
+        <>
+          <Modal />
+          <Message />
+          <Routes>
+            <Route path="/categories/:categoryId/items" element={<Items />} />
+          </Routes>
+          ,
+        </>,
+        { user: { isLoggedIn: true } },
+        undefined,
+        `/categories/${categoryId}/items`,
+      );
+
+      await waitFor(() => {
+        expect(
+          screen.getByRole('columnheader', {
+            name: /id/i,
+          }),
+        ).toBeInTheDocument();
+      });
+
+      await userEvent.click(screen.getAllByRole('button', { name: /edit item/i })[0]);
+
+      await waitFor(() => {
+        expect(screen.getByText(/edit item form/i)).toBeInTheDocument();
+      });
+    });
+
+    test('user is not logged in', async () => {
+      renderWithProviders(
+        <>
+          <Modal />
+          <Message />
+          <Routes>
+            <Route path="/categories/:categoryId/items" element={<Items />} />
+          </Routes>
+          ,
+        </>,
+        { user: { isLoggedIn: false } },
+        undefined,
+        `/categories/${categoryId}/items`,
+      );
+
+      await waitFor(() => {
+        expect(
+          screen.getByRole('columnheader', {
+            name: /id/i,
+          }),
+        ).toBeInTheDocument();
+      });
+
+      await userEvent.click(screen.getAllByRole('button', { name: /edit item/i })[0]);
+
+      await waitFor(() => {
+        expect(screen.getByText(/authentication warning/i)).toBeInTheDocument();
+      });
+    });
+  });
+
+  describe('call api', () => {
+    test('edit successfully', async () => {
+      // Edit category with id 1
+      const editItemActionSpy = jest.spyOn(itemAction, 'editItem');
+      renderWithProviders(
+        <>
+          <Modal />
+          <Message />
+          <Routes>
+            <Route path="/categories/:categoryId/items" element={<Items />} />
+          </Routes>
+          ,
+        </>,
+        { user: { isLoggedIn: true } },
+        undefined,
+        `/categories/${categoryId}/items`,
+      );
+
+      await waitFor(() => {
+        expect(
+          screen.getByRole('columnheader', {
+            name: /id/i,
+          }),
+        ).toBeInTheDocument();
+      });
+
+      await userEvent.click(screen.getAllByRole('button', { name: /edit item/i })[0]);
+
+      await waitFor(() => {
+        expect(screen.getByText(/edit item form/i)).toBeInTheDocument();
+      });
+
+      const data = {
+        imageUrl: TEST_IMAGE_URL,
+        description: 'item description test',
+      };
+
+      await userEvent.clear(screen.getByPlaceholderText(/image url/i));
+      await userEvent.type(screen.getByPlaceholderText(/image url/i), data.imageUrl);
+
+      await userEvent.clear(screen.getByPlaceholderText(/description/i));
+      await userEvent.type(screen.getByPlaceholderText(/description/i), data.description);
+
+      await userEvent.click(screen.getByRole('button', { name: /submit/i }));
+
+      await waitFor(() => {
+        expect(screen.getByText(/edit item successfully/i)).toBeInTheDocument();
+      });
+
+      // Check if item is updated on the UI
+      expect(screen.getByRole('cell', { name: /item description test/i })).toBeInTheDocument();
+
+      expect(editItemActionSpy).toBeCalledTimes(1);
+      expect(editItemActionSpy).toHaveBeenCalledWith(+categoryId, 1, expect.objectContaining(data));
+
+      editItemActionSpy.mockRestore();
+    });
+  });
+});
+
+describe('remove category', () => {
+  describe('open modal', () => {
+    test('user is logged in', async () => {
+      renderWithProviders(
+        <>
+          <Modal />
+          <Message />
+          <Routes>
+            <Route path="/categories/:categoryId/items" element={<Items />} />
+          </Routes>
+          ,
+        </>,
+        { user: { isLoggedIn: true } },
+        undefined,
+        `/categories/${categoryId}/items`,
+      );
+
+      await waitFor(() => {
+        expect(
+          screen.getByRole('columnheader', {
+            name: /id/i,
+          }),
+        ).toBeInTheDocument();
+      });
+
+      await userEvent.click(screen.getAllByRole('button', { name: /remove item/i })[0]);
+
+      await waitFor(() => {
+        expect(screen.getByText(/delete warning/i)).toBeInTheDocument();
+      });
+    });
+
+    test('user is not logged in', async () => {
+      renderWithProviders(
+        <>
+          <Modal />
+          <Message />
+          <Routes>
+            <Route path="/categories/:categoryId/items" element={<Items />} />
+          </Routes>
+          ,
+        </>,
+        { user: { isLoggedIn: false } },
+        undefined,
+        `/categories/${categoryId}/items`,
+      );
+
+      await waitFor(() => {
+        expect(
+          screen.getByRole('columnheader', {
+            name: /id/i,
+          }),
+        ).toBeInTheDocument();
+      });
+
+      await userEvent.click(screen.getAllByRole('button', { name: /edit item/i })[0]);
+
+      await waitFor(() => {
+        expect(screen.getByText(/authentication warning/i)).toBeInTheDocument();
+      });
+    });
+  });
+
+  describe('call api', () => {
+    test('remove successfully', async () => {
+      // Edit category with id 1
+      const deleteItemSpyOn = jest.spyOn(itemAction, 'removeItem');
+      renderWithProviders(
+        <>
+          <Modal />
+          <Message />
+          <Routes>
+            <Route path="/categories/:categoryId/items" element={<Items />} />
+          </Routes>
+          ,
+        </>,
+        { user: { isLoggedIn: true } },
+        undefined,
+        `/categories/${categoryId}/items`,
+      );
+
+      await waitFor(() => {
+        expect(
+          screen.getByRole('columnheader', {
+            name: /id/i,
+          }),
+        ).toBeInTheDocument();
+      });
+
+      await userEvent.click(screen.getAllByRole('button', { name: /remove item/i })[0]);
+
+      await waitFor(() => {
+        expect(screen.getByText(/delete warning/i)).toBeInTheDocument();
+      });
+
+      await userEvent.click(screen.getByRole('button', { name: /confirm/i }));
+
+      await waitFor(() => {
+        expect(screen.getByText(/delete item successfully/i)).toBeInTheDocument();
+      });
+
+      // Check if item is updated on the UI
+      expect(screen.queryByRole('cell', { name: /item 1 description/i })).toBeNull();
+
+      expect(deleteItemSpyOn).toBeCalledTimes(1);
+      expect(deleteItemSpyOn).toBeCalledWith(+categoryId, 1);
+
+      deleteItemSpyOn.mockRestore();
     });
   });
 });
